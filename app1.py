0fd593be411e09c14da2952d89b62f4df1cf1492
CabangPurnama <117652095+CabangPurnama@users.noreply.github.com>
2022-11-22 15:20:16 +0700

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,24 +1,14 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
-import os
-from datetime import datetime
-from tempfile import NamedTemporaryFile
 
-
-def export(request):
-    response = Response(content_type='application/csv')
-    with NamedTemporaryFile(prefix='XML_Export_%s' % datetime.now(),
-                            suffix='.xml', delete=True) as f:
-        # this is where I usually put stuff in the file
-        response = FileResponse(os.path.abspath(f.name))
-        response.headers['Content-Disposition'] = ("attachment; filename=Cars.xml")
-        return response
+def hello_world(request):
+    return Response('Hello World!')
 
 if __name__ == '__main__':
     with Configurator() as config:
-        config.add_route('Cars', '/')
-        config.add_view( route_name='Cars')
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
